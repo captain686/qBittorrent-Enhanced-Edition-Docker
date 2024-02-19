68ca0c253beeb5ad8ebd3d611f13dde6a99993c9
captain_686 <53248484+captain686@users.noreply.github.com>
2024-02-19 10:43:47 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,8 +2,6 @@
 
 last_version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
-echo $last_version
-
 if [ -f "qbittorrent-nox_version" ];then
     version=$(cat qbittorrent-nox_version)
     if [ "$version" != "$last_version" ];then
