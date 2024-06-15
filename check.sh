b2d03c6eebca04327725eb51362cdee55d62bc68
captain686 <53248484+captain686@users.noreply.github.com>
2024-06-15 19:48:41 +0800

    🤔check releases_status

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 last_version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
-releases_status=$("curl -sIL -w "%{http_code}" -o /dev/null https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/$last_version/qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip")
+releases_status=$(curl -sIL -w "%{http_code}" -o /dev/null https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/$last_version/qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip)
 
 if [ -f "qbittorrent-nox_version" ];then
     version=$(cat qbittorrent-nox_version)
